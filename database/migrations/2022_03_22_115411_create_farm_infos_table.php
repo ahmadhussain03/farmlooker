ee7f12dbe06d276064964ad82f79ae76084910b5
Ahmad <ahmadhussain03@hotmail.com>
2022-03-22 16:55:38 +0500

    Farm Info Migration & Model Added

---
+++ database/migrations/2022_03_22_115411_create_farm_infos_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFarmInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('farm_infos', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('t_min');
+            $table->string('t_max');
+            $table->string('wind_speed');
+            $table->string('cloud_cover');
+            $table->string('humidity');
+            $table->string('rainfall');
+            $table->string('msavi');
+            $table->string('ndre');
+            $table->string('recl');
+            $table->string('ndvi');
+
+            $table->foreignId('farm_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('farm_infos', function (Blueprint $table){
+            $table->dropForeign(['farm_id']);
+        });
+        Schema::dropIfExists('farm_infos');
+    }
+}
