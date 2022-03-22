8d0f55e9206df63e54ab3c9057b204fa0978d02b
Ahmad <ahmadhussain03@hotmail.com>
2022-03-22 16:52:48 +0500

    job created

---
+++ app/Jobs/DailyFarmInfo.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Farm;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class DailyFarmInfo implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Farm::chunk(50, function($farms){
+            foreach($farms as $farm){
+                // $farm->farmInfos()->create([
+
+                // ]);
+            }
+        });
+    }
+}
